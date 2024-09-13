640f0b12b0adf772fc731cc49be9a448ec3e83af
jhshin <jhshin@os.korea.ac.kr>
2024-09-14 01:06:35 +0900

    [VehicleSpeed] sync with vehicle simulator

---
+++ services/VehicleSpeed/CMakeFiles/VehicleSpeed_client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VehicleSpeed_client.dir/src-gen/v1/commonapi/VehicleSpeedSomeIPDeployment.cpp.o"
+  "CMakeFiles/VehicleSpeed_client.dir/src-gen/v1/commonapi/VehicleSpeedSomeIPProxy.cpp.o"
+  "CMakeFiles/VehicleSpeed_client.dir/src/VehicleSpeed_client.cpp.o"
+  "VehicleSpeed_client"
+  "VehicleSpeed_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VehicleSpeed_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ services/VehicleSpeed/CMakeFiles/VehicleSpeed_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VehicleSpeed_server.dir/src-gen/v1/commonapi/VehicleSpeedSomeIPDeployment.cpp.o"
+  "CMakeFiles/VehicleSpeed_server.dir/src-gen/v1/commonapi/VehicleSpeedSomeIPStubAdapter.cpp.o"
+  "CMakeFiles/VehicleSpeed_server.dir/src/VehicleSpeed_server.cpp.o"
+  "CMakeFiles/VehicleSpeed_server.dir/src/VehicleSpeed_server_stubimpl.cpp.o"
+  "VehicleSpeed_server"
+  "VehicleSpeed_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VehicleSpeed_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
