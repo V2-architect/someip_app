e88ce9b85bd1d7ef5e6436c0b85c7327efd4bba9
jhshin <jhshin@os.korea.ac.kr>
2024-09-15 00:07:37 +0900

    add rule to separate VehicleService/routingmanager

--- services/deploy.py
+++ services/deploy.py
@@ -14,15 +14,22 @@ def main():
     SERVICE_NAME = items[num-1]
     print(f"SERVICE_NAME: {SERVICE_NAME}")
 
-    os.system(f"sudo docker cp someip_dev:/home/jhshin/work/someip_dev/services/{SERVICE_NAME}/release/ .")
-    os.system(f"mv release/ {SERVICE_NAME}")
-    os.system(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
+    if os.path.exists(SERVICE_NAME):
+        os.system(f"rm -rf {SERVICE_NAME}")
 
-    '''
-    print(f"sudo docker cp someip_dev:/home/jhshin/work/someip_dev/services/{SERVICE_NAME}/release/ .")
-    print(f"mv release/ {SERVICE_NAME}")
-    print(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
-    '''
+    if SERVICE_NAME == "routingmanager":
+        os.system(f"sudo docker cp someip_dev:/home/jhshin/work/someip_dev/services/{SERVICE_NAME}/ .")
+        os.system(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
+    else:
+        os.system(f"sudo docker cp someip_dev:/home/jhshin/work/someip_dev/services/{SERVICE_NAME}/release/ .")
+        os.system(f"mv release/ {SERVICE_NAME}")
+        os.system(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
+
+        '''
+        print(f"sudo docker cp someip_dev:/home/jhshin/work/someip_dev/services/{SERVICE_NAME}/release/ .")
+        print(f"mv release/ {SERVICE_NAME}")
+        print(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
+        '''
 
 if __name__ == "__main__":
     main()
