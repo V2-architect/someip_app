3adb25cf6c718c80cd218565bafe25aa84a44825
jhshin <jhshin@os.korea.ac.kr>
2024-09-15 14:12:15 +0900

    SOME/IP SERVICE release
    - VehicleSpeed
    - VehiclePose
    - VehicleAccel

--- services/VehicleSpeed/run_client.sh
+++ services/VehicleSpeed/run_client.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -e
 
-if [ -z "$1" -o -z "$2" ]; then
+if [ -z "$1" ]; then
     echo "[Error] input the argument like below"
-    echo "  $ $0 <HOST_IP> <TRANS_PROTOCOL>"
-    echo "  $ $0 10.0.0.1 tcp"
-    echo "  $ $0 10.0.0.1 udp"
+    echo "  $ $0 <TRANS_PROTOCOL> [<delay(second),default(5s)>]"
+    echo "  $ $0 tcp 5"
+    echo "  $ $0 udp 5"
     exit -1
 fi
 
+# set curr_path to script_path
+CURR_SH_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${CURR_SH_PATH}
+
+LAUNCH_DELAY=$2
+if [ -z "$2" ]; then
+	LAUNCH_DELAY=5
+fi
+echo "LAUNCH_DELAY: "${LAUNCH_DELAY}
+sleep ${LAUNCH_DELAY}
+
+
 # get data from user input
-SOMEIP_HOST_IP=$1
-TRANSPORT_PROTOCOL=$2
+HOSTNAME=$(hostname)
+SOMEIP_HOST_IP=`cat ../routingmanager/host_ip.txt | grep $(hostname) | awk -F: '{print $2}'`
+TRANSPORT_PROTOCOL=$1
 
 DEV_MODE="n"
 if [ "$(echo $(pwd) | grep someip_dev)" != "" ]; then
@@ -18,7 +31,7 @@ if [ "$(echo $(pwd) | grep someip_dev)" != "" ]; then
 fi
 
 # get SOME/IP service runtime information from someip_service.prop
-APP_NAME=`cat someip_service.prop | grep APP_NAME | awk -F= '{print $2}'`
+SERVICE_NAME=`cat someip_service.prop | grep SERVICE_NAME | awk -F= '{print $2}'`
 MULTICAST_IP=`cat someip_service.prop | grep MULTICAST_IP | awk -F= '{print $2}'`
 
 # get env variable at runtime
@@ -30,16 +43,20 @@ CAPICXX_SOMEIP_PATH=${GIT_PROJECT_ROOT}/dependencies/capicxx-someip-runtime_3.2.
 VSOMEIP_PATH=${GIT_PROJECT_ROOT}/dependencies/vsomeip_3.3.0/lib
 APP_LD_LIBRARY_PATH=${BOOST_LIBRARY_PATH}:${CAPICXX_CORE_PATH}:${CAPICXX_SOMEIP_PATH}:${VSOMEIP_PATH}
 
-APP_ROOT=${GIT_PROJECT_ROOT}/services/${APP_NAME}
+APP_ROOT=${GIT_PROJECT_ROOT}/services/${SERVICE_NAME}
 if [ "${DEV_MODE}" == "y" ]; then
-    APP_ROOT=${GIT_PROJECT_ROOT}/services/${APP_NAME}/build
+    APP_ROOT=${GIT_PROJECT_ROOT}/services/${SERVICE_NAME}/build
 fi
 
 # check prerequisite
 ./pre_check.py ${APP_LD_LIBRARY_PATH}
 
-CONF_FILE_TEMPLATE=${APP_ROOT}/client_$TRANSPORT_PROTOCOL.json.in
-CONF_FILE=${APP_ROOT}/client_$TRANSPORT_PROTOCOL.json
+#CONF_FILE_TEMPLATE=${APP_ROOT}/client_$TRANSPORT_PROTOCOL.json.in
+#CONF_FILE=${APP_ROOT}/client_$TRANSPORT_PROTOCOL.json
+CONF_FILE_TEMPLATE=${APP_ROOT}/client.json.in
+CONF_FILE=${APP_ROOT}/client.json
+
+
 
 echo "==========================================="
 echo "============= SOME/IP Client =============="
@@ -54,11 +71,8 @@ echo "LD_LIBRARY_PATH   : "${APP_LD_LIBRARY_PATH}
 
 sed "s/@SOMEIP_HOST_IP@/${SOMEIP_HOST_IP}/g" $CONF_FILE_TEMPLATE > $CONF_FILE
 
-#LD_LIBRARY_PATH=$APP_LD_LIBRARY_PATH \
-#VSOMEIP_CONFIGURATION=$CONF_FILE \
-#    VSOMEIP_APPLICATION_NAME=$APP_NAME \
-#    ${APP_ROOT}/${APP_NAME}_client_$TRANSPORT_PROTOCOL
-
 LD_LIBRARY_PATH=$APP_LD_LIBRARY_PATH \
-    VSOMEIP_APPLICATION_NAME=$APP_NAME \
-    ${APP_ROOT}/${APP_NAME}_client_$TRANSPORT_PROTOCOL
+VSOMEIP_CONFIGURATION=$CONF_FILE \
+    VSOMEIP_APPLICATION_NAME=$SERVICE_NAME \
+    ${APP_ROOT}/${SERVICE_NAME}_client_$TRANSPORT_PROTOCOL
+
