ef5ce26d91870b479f2f862f47c9aa26e0bf7f45
jhshin <jhshin@os.korea.ac.kr>
2024-09-22 01:24:58 +0900

    add options 'all' of deploy.py

--- services/deploy.py
+++ services/deploy.py
@@ -2,18 +2,9 @@
 
 import sys
 import os
+import pdb
 
-def main():
-    items = os.popen("sudo docker exec someip_dev ls /home/jhshin/work/someip_dev/services").read().strip().split("\n")
-    for i, item in enumerate(items, 1):
-        print(f"[{i}] {item}")
-
-    num = input("choose the number >> ")
-    num = int(num)
-
-    SERVICE_NAME = items[num-1]
-    print(f"SERVICE_NAME: {SERVICE_NAME}")
-
+def deploy(SERVICE_NAME):
     if os.path.exists(SERVICE_NAME):
         os.system(f"rm -rf {SERVICE_NAME}")
 
@@ -31,5 +22,33 @@ def main():
         print(f"sudo chown -R jhshin:jhshin {SERVICE_NAME}")
         '''
 
+def deploy_all():
+    service_list = ["Collision", "Driving", "Intersection", "ObjectDetection", "SteeringWheel", "TrafficLight", "Transmission", "VehicleAccel", "VehicleLocation", "VehiclePose", "VehicleSpeed"]
+
+    for svc in service_list:
+        print(f"Copy someip_dev/ to current directory ({svc})")
+        deploy(svc)
+
+
+def main():
+    items = os.popen("sudo docker exec someip_dev ls /home/jhshin/work/someip_dev/services").read().strip().split("\n")
+    for i, item in enumerate(items, 1):
+        print(f"[{i}] {item}")
+
+    num = input("choose the number >> ")
+    num = int(num)
+
+    SERVICE_NAME = items[num-1]
+    print(f"SERVICE_NAME: {SERVICE_NAME}")
+
+    deploy(SERVICE_NAME)
+
+    # deploy time_interval txt
+    os.system("python3 ./set_svc_event_interval.py")
+
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == 'all':
+        deploy_all()
+    else:
+        main()
