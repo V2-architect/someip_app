a6d823dcebeb303e8557068edf78edfe52ecb366
jhshin <jhshin@os.korea.ac.kr>
2024-09-25 01:13:20 +0900

    automation script to run routing/server/client

---
+++ scripts/cc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+/root/someip_app/scripts/client_${HOSTNAME}.sh
---
+++ scripts/ss.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+/root/someip_app/scripts/server_${HOSTNAME}.sh
