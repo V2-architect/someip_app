94497b64ed1944427a62010d3b61808278751449
jhshin <jhshin@os.korea.ac.kr>
2024-10-09 14:42:16 +0900

    add delay(10s) before merging pcaps
    - missing some packets when merging pcap files right after stop tcpdumps
    - so, add 10s delay before merging pcaps

--- scripts/start_multi_capture.sh
+++ scripts/start_multi_capture.sh
@@ -13,3 +13,12 @@ sudo tcpdump -i s1-eth6 ether src 00:00:00:00:00:06 -w eth6.pcap &
 sudo tcpdump -i s1-eth7 ether src 00:00:00:00:00:07 -w eth7.pcap &
 sudo tcpdump -i s1-eth8 ether src 00:00:00:00:00:08 -w eth8.pcap &
 
+#sudo tshark -i s1-eth1 -t ad -f "inbound" -w /tmp/eth1.pcap &
+#sudo tshark -i s1-eth2 -t ad -f "inbound" -w /tmp/eth2.pcap &
+#sudo tshark -i s1-eth3 -t ad -f "inbound" -w /tmp/eth3.pcap &
+#sudo tshark -i s1-eth4 -t ad -f "inbound" -w /tmp/eth4.pcap &
+#sudo tshark -i s1-eth5 -t ad -f "inbound" -w /tmp/eth5.pcap &
+#sudo tshark -i s1-eth6 -t ad -f "inbound" -w /tmp/eth6.pcap &
+#sudo tshark -i s1-eth7 -t ad -f "inbound" -w /tmp/eth7.pcap &
+#sudo tshark -i s1-eth8 -t ad -f "inbound" -w /tmp/eth8.pcap &
+
--- scripts/stop_multi_capture.sh
+++ scripts/stop_multi_capture.sh
@@ -5,15 +5,30 @@ PCAP_FILE="inbound-${CURR_DATE}.pcap"
 echo "CURR_DATE: "${CURR_DATE}
 
 echo "kill all tcpdump process - start"
-sudo kill -9 `ps -ef | grep 's1-eth1 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth2 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth3 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth4 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth5 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth6 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth7 ether src' | grep -v grep | awk '{print $2}'`
-sudo kill -9 `ps -ef | grep 's1-eth8 ether src' | grep -v grep | awk '{print $2}'`
+sudo pkill tcpdump
+#sudo kill -9 `ps -ef | grep 's1-eth1 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth2 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth3 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth4 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth5 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth6 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth7 ether src' | grep -v grep | awk '{print $2}'`
+#sudo kill -9 `ps -ef | grep 's1-eth8 ether src' | grep -v grep | awk '{print $2}'`
 echo "kill all tcpdump process - end"
+
+
+#echo "kill all tshark process - start"
+#sudo pkill tshark
+#echo "kill all tshark process - end"
+
+#echo "copy /tmp/eth*.pcap to curr dir"
+#sudo cp -rf /tmp/eth*.pcap .
+#sudo rm -rf /tmp/eth*.pcap
+
+#echo "chown jhshin:jhshin eth*.pcap"
+#sudo chown jhshin:jhshin eth*.pcap
+
+sleep 10
 
 echo "merge all *.pcap files -> merged.pcap - start"
 sudo mergecap -w merged.pcap \
@@ -30,4 +45,4 @@ echo "merge all *.pcap files -> merged.pcap - end"
 echo "rename merged.pcap to ${PCAP_FILE}"
 mv merged.pcap ${PCAP_FILE}
 
-#sudo chown -R jhshin:jhshin *.pcap
+sudo chown -R jhshin:jhshin *.pcap
